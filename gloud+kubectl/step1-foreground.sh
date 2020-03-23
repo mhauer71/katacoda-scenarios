5cc7bd76ffbe89fe0bc90d92d27ff620fa60cbd0
Marcos Paulo Hauer <mhauer@gmail.com>
2020-03-23 17:54:21 -0300

    Change image to kubernetes

--- gloud+kubectl/step1-foreground.sh
+++ gloud+kubectl/step1-foreground.sh
@@ -4,14 +4,7 @@
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get -y install google-cloud-sdk.
-
-#  kubectl e jq
-sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl jq
+sudo apt-get update && sudo apt-get -y install google-cloud-sdk jq
 
 #  kubectx
 git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
