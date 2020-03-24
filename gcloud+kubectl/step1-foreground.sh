64de97ff0321f8de4b22a095544fa36384ac5ee2
Marcos Paulo Hauer <mhauer@gmail.com>
2020-03-24 09:36:26 -0300

    Fix kubectx script

--- gcloud+kubectl/step1-foreground.sh
+++ gcloud+kubectl/step1-foreground.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
+
+install_kubectx () {
+    
+}
 # Script para instalar dependências
 #  gcloud cli, jq, pkg-config
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get -y install google-cloud-sdk jq pkg-config
+sudo apt-get update && sudo apt-get -y install google-cloud-sdk jq
 
 #  kubectx
 git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
-COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
 ln -sf ~/.kubectx/completion/kubens.bash $COMPDIR/kubens
 ln -sf ~/.kubectx/completion/kubectx.bash $COMPDIR/kubectx
-cat << FOE >> ~/.bashrc
-#kubectx and kubens
-export PATH=~/.kubectx:\$PATH
-FOE
+export PATH=~/.kubectx:$PATH
